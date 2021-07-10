8e2f2d13f0e3f20a26d919d714bc0789fad62a4f
Ibrahim Ahmed <ibraahim6@icloud.com>
2021-07-10 08:45:44 +0200

    testing end point and api key

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,37 @@
 <template>
   <div class="home">
-    Hello in App
+    <div v-for="video in videos" :key="video">
+      <pre>
+      {{video}}
+      </pre>
+    </div>
   </div>
 </template>
 
 <script>
-
+import { apiClient } from "@/config";
 export default {
   name: "Home",
   components: {
+  },
+  data(){
+    return{
+      videos:[]
+    };
+  },
+  created(){
+    this.fetchData();
+  },
+  methods: {
+    fetchData(){
+      apiClient
+            .get(
+              `/search?part=snippet&q=spongebob`
+            )
+            .then((res) => {
+              this.videos = res.data;
+            });
+    }
   },
 };
 </script>
